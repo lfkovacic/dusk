bbcc60f1e484616c888130ff6318823758b1f1e1
L. Filip Kovačić <lfkovacic@gmail.com>
2024-12-24 22:01:33 +0100

    triangle(s)!

--- Game1.cs
+++ Game1.cs
@@ -7,7 +7,8 @@ namespace dusk;
 public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+    private BasicEffect _basicEffect;
+    private VertexPositionColor[] _triangleVertices;
 
     public Game1()
     {
@@ -18,24 +19,43 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
-    }
 
-    protected override void LoadContent()
-    {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        // Define the vertices for the triangle outline (two vertices per edge)
+        _triangleVertices = new VertexPositionColor[6];
+        _triangleVertices[0] = new VertexPositionColor(new Vector3(100, 200, 0), Color.Red); // Bottom-left
+        _triangleVertices[1] = new VertexPositionColor(new Vector3(300, 200, 0), Color.Red); // Bottom-right
 
-        // TODO: use this.Content to load your game content here
+        _triangleVertices[2] = new VertexPositionColor(new Vector3(300, 200, 0), Color.Green); // Bottom-right
+        _triangleVertices[3] = new VertexPositionColor(new Vector3(200, 100, 0), Color.Green); // Top
+
+        _triangleVertices[4] = new VertexPositionColor(new Vector3(200, 100, 0), Color.Blue); // Top
+        _triangleVertices[5] = new VertexPositionColor(new Vector3(100, 200, 0), Color.Blue); // Bottom-left
+
+        // Set up the BasicEffect
+        _basicEffect = new BasicEffect(GraphicsDevice)
+        {
+            VertexColorEnabled = true,
+            Projection = Matrix.CreateOrthographicOffCenter(0, GraphicsDevice.Viewport.Width,
+                                                            GraphicsDevice.Viewport.Height, 0, 0, 1),
+            World = Matrix.Identity,
+            View = Matrix.Identity
+        };
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        // Get the current mouse state
+        var mouseState = Mouse.GetState();
 
-        // TODO: Add your update logic here
+        // Check if the left mouse button is pressed
+        if (mouseState.LeftButton == ButtonState.Pressed)
+        {
+            // Update the position of the "Top" vertex (index 3 and 4 in the array)
+            Vector3 mousePosition = new Vector3(mouseState.X, mouseState.Y, 0);
+            _triangleVertices[3].Position = mousePosition; // Update the top vertex
+            _triangleVertices[4].Position = mousePosition; // Also update the connected edge
+        }
 
         base.Update(gameTime);
     }
@@ -44,8 +64,13 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        foreach (var pass in _basicEffect.CurrentTechnique.Passes)
+        {
+            pass.Apply();
+            GraphicsDevice.DrawUserPrimitives(PrimitiveType.LineList, _triangleVertices, 0, 3); // Three lines
+        }
 
         base.Draw(gameTime);
     }
+
 }
